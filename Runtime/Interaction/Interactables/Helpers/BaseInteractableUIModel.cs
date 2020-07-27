6b99c931593642e6418a779b6bb0330daf4ac210
Chad Kilgore <ckilgore@gmail.com>
2020-07-26 21:39:23 -0500

    ability to invoke changed event

--- Runtime/Interaction/Interactables/Helpers/BaseInteractableUIModel.cs
+++ Runtime/Interaction/Interactables/Helpers/BaseInteractableUIModel.cs
@@ -5,5 +5,10 @@ namespace UnityEngine.Interaction.Toolkit.UI
 	public abstract class BaseInteractableUIModel : MonoBehaviour
 	{
 		public event System.EventHandler onChanged;
+		
+		protected void InvokeChanged()
+		{
+			onChanged?.Invoke(this, System.EventArgs.Empty);
+		}
 	}
 }
